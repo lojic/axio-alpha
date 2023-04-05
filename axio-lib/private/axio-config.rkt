91b07ed39209ccdb1a8a46cd08f060dd4a53f602
Brian Adkins <brian@lojic.com>
2023-04-04 21:12:52 -0400

    Add getters

--- axio-lib/private/axio-config.rkt
+++ axio-lib/private/axio-config.rkt
@@ -7,11 +7,11 @@
             (->* (axio-config?)
                  (#:db-config   (or/c axio-db-config?   #f)
                   #:smtp-config (or/c axio-smtp-config? #f))
-                 any) ])
-         a-axio-config
-         a-axio-db-config
-         a-axio-smtp-config
-         axio-worker-custodian
+                 any) ]
+          [ get-axio-config           (-> axio-config?)      ]
+          [ get-axio-db-config        (-> axio-db-config?)   ]
+          [ get-axio-smtp-config      (-> axio-smtp-config?) ]
+          [ get-axio-worker-custodian (-> custodian?)        ])
          (struct-out axio-config)
          (struct-out axio-db-config)
          (struct-out axio-smtp-config))
@@ -37,3 +37,8 @@
   (set! a-axio-config config)
   (set! a-axio-db-config db-config)
   (set! a-axio-smtp-config smtp-config))
+
+(define (get-axio-config)           a-axio-config)
+(define (get-axio-db-config)        a-axio-db-config)
+(define (get-axio-smtp-config)      a-axio-smtp-config)
+(define (get-axio-worker-custodian) axio-worker-custodian)
