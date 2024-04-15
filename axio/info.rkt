1b25d63f9880a9e204fbc1b649f20b164195951f
D. Ben Knoble <ben.knoble+github@gmail.com>
2024-04-15 14:55:52 -0400

    axio: fix axio-lib dep

--- axio/info.rkt
+++ axio/info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define collection "axio")
-(define deps '("axio-lib" "base"))
+(define deps '("git://github.com/lojic/axio-alpha?path=axio-lib"
+               "base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/axio.scrbl" ())))
 (define pkg-desc "Axio Web Framework")
